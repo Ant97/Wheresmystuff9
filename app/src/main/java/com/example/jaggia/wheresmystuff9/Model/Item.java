06631cfd63ce09c29daf60e6e416a2e6c79ffbb8
Anadi Jaggia <jaggia@lawn-143-215-90-91.lawn.gatech.edu>
2017-06-22 17:56:59 -0400

    added Javadocs

--- app/src/main/java/com/example/jaggia/wheresmystuff9/Model/Item.java
+++ app/src/main/java/com/example/jaggia/wheresmystuff9/Model/Item.java
@@ -22,83 +22,161 @@ public class Item {
     private ItemStatus status;
     private ItemType type;
 
+    /**
+     * @return current User
+     */
     public User getUser() {
         return user;
     }
 
+    /**
+     * Set current user to the paramater 'user'
+     *
+     * @param user
+     */
     public void setUser(User user) {
         this.user = user;
     }
 
+    /**
+     * @return current name for the Item
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Set current Item name to the paramater 'name'
+     *
+     * @param name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * @return current description for the Item
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Set current Item description to the paramater 'description'
+     *
+     * @param description
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
+    /**
+     * @return current date for the Item
+     */
     public Date getDate() {
         return date;
     }
 
+    /**
+     * Set current Item date to the paramater 'date'
+     *
+     * @param date
+     */
     public void setDate(Date date) {
         this.date = date;
     }
 
+    /**
+     * @return current Location for the Item
+     */
     public Location getLocation() {
         return location;
     }
 
+    /**
+     * Set current Item location to the paramater 'location'
+     *
+     * @param location
+     */
     public void setLocation(Location location) {
         this.location = location;
     }
 
+    /**
+     * @return current category for the Item
+     */
     public String getCategory() {
         return category;
     }
 
+    /**
+     * Set current Item category to the paramater 'category'
+     *
+     * @param category
+     */
     public void setCategory(String category) {
         this.category = category;
     }
 
-
-
+    /**
+     * @return current reward for the Item
+     */
     public int getReward() {
         return reward;
     }
 
+    /**
+     * Set current Item reward to the paramater 'reward'
+     *
+     * @param reward
+     */
     public void setReward(int reward) {
         this.reward = reward;
     }
 
+    /**
+     * @return current status for the Item
+     */
     public ItemStatus getStatus() {
         return status;
     }
 
+    /**
+     * Set current Item status to the paramater 'status'
+     *
+     * @param status
+     */
     public void setStatus(ItemStatus status) {
         this.status = status;
     }
 
+
+    /**
+     * @return current type for the Item
+     */
     public ItemType getType() {
         return type;
     }
 
+    /**
+     * Set current Item type to the paramater 'type'
+     *
+     * @param type
+     */
     public void setType(ItemType type) {
         this.type = type;
     }
 
+    /**
+     * Set current Item resolved status to RESOLVED
+     */
     public void setResolved() {
         setStatus(ItemStatus.RESOLVED);
     }
+
+    /**
+     * Set current Item resolved status to UNRESOLVED
+     */
     public void setUnResolved() {
         setStatus(ItemStatus.UNRESOLVED);
     }
