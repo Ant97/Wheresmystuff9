53bb7c21dac30bccbb37300034b2ea58ab0c4fd1
Anadi Jaggia <jaggia@lawn-143-215-90-91.lawn.gatech.edu>
2017-06-22 17:31:45 -0400

    Fixed Item.java

--- app/src/main/java/com/example/jaggia/wheresmystuff9/Model/Item.java
+++ app/src/main/java/com/example/jaggia/wheresmystuff9/Model/Item.java
@@ -9,7 +9,8 @@ import java.util.Date;
 
 public class Item {
 
-    private enum ItemStatus { LOST, FOUND};
+    private enum ItemStatus { RESOLVED, UNRESOLVED};
+    private enum ItemType { LOST, FOUND};
 
     private User user;
     private String name;
@@ -17,81 +18,88 @@ public class Item {
     private Date date;
     private Location location;
     private String category;
-    private String type;
     private int reward;
     private ItemStatus status;
+    private ItemType type;
 
-    /** ********************************************
-     * the getters and setters
-     */
     public User getUser() {
         return user;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public Location getLocation() {
-        return location;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public ItemStatus getStatus() {
-        return status;
     }
 
     public void setUser(User user) {
         this.user = user;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public Date getDate() {
+        return date;
     }
 
     public void setDate(Date date) {
         this.date = date;
     }
 
+    public Location getLocation() {
+        return location;
+    }
+
     public void setLocation(Location location) {
         this.location = location;
+    }
+
+    public String getCategory() {
+        return category;
     }
 
     public void setCategory(String category) {
         this.category = category;
     }
 
-    public void setType(String type) {
-        this.type = type;
+
+
+    public int getReward() {
+        return reward;
+    }
+
+    public void setReward(int reward) {
+        this.reward = reward;
+    }
+
+    public ItemStatus getStatus() {
+        return status;
     }
 
     public void setStatus(ItemStatus status) {
         this.status = status;
     }
 
+    public ItemType getType() {
+        return type;
+    }
+
+    public void setType(ItemType type) {
+        this.type = type;
+    }
+
     public void setResolved() {
-        setStatus(ItemStatus.FOUND);
+        setStatus(ItemStatus.RESOLVED);
     }
     public void setUnResolved() {
-        setStatus(ItemStatus.LOST);
+        setStatus(ItemStatus.UNRESOLVED);
     }
 }
