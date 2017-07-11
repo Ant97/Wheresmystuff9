768b4ac51fc7f8bb71aa8f26304b127302288a3e
James <jgormely6@gatech.edu>
2017-07-10 22:33:31 -0400

    Linking MAP and DATABASE changes

--- app/src/main/java/com/example/jaggia/wheresmystuff9/controllers/MapViewPosts.java
+++ app/src/main/java/com/example/jaggia/wheresmystuff9/controllers/MapViewPosts.java
@@ -12,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.example.jaggia.wheresmystuff9.R;
 
 public class MapViewPosts extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
